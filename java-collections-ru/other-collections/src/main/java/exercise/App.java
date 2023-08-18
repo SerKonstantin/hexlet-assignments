14bf238de469b23eba652dbfefae702a1866bf42
@hexlet/cli <support@hexlet.io>
2023-08-18 23:27:01 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -8,31 +8,18 @@ import java.util.Set;
 
 // BEGIN
 public class App {
-    // Made everything like this because lesson theme was "using Sets"
-    // From the first glance it would be simpler to merge keys in one list then do if/else checks with contains
-
     public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
-        Set<String> keys1 = new HashSet<>(data1.keySet());
-        Set<String> keys2 = new HashSet<>(data2.keySet());
+        Set<String> combinedKeys = new HashSet<>(data1.keySet());
+        combinedKeys.addAll(data2.keySet());
 
         Map<String, String> results = new LinkedHashMap<>();
 
-        Set<String> added = new HashSet<>(keys2);
-        added.removeAll(keys1);
-        for (String key: added) {
-            results.put(key, "added");
-        }
-
-        Set<String> deleted = new HashSet<>(keys1);
-        deleted.removeAll(keys2);
-        for (String key: deleted) {
-            results.put(key, "deleted");
-        }
-
-        Set<String> intersection = new HashSet<>(keys1);
-        intersection.retainAll(keys2);
-        for (String key: intersection) {
-            if (data1.get(key).equals(data2.get(key))) {
+        for (var key: combinedKeys) {
+            if (!data1.containsKey(key)) {
+                results.put(key, "added");
+            } else if (!data2.containsKey(key)) {
+                results.put(key, "deleted");
+            } else if (data1.get(key).equals(data2.get(key))) {
                 results.put(key, "unchanged");
             } else {
                 results.put(key, "changed");
@@ -41,5 +28,40 @@ public class App {
 
         return results;
     }
+
+
+    //Another approach just for study purposes, because lesson theme was "using Sets"
+
+//    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+//        Set<String> keys1 = new HashSet<>(data1.keySet());
+//        Set<String> keys2 = new HashSet<>(data2.keySet());
+//
+//        Map<String, String> results = new LinkedHashMap<>();
+//
+//        Set<String> added = new HashSet<>(keys2);
+//        added.removeAll(keys1);
+//        for (String key: added) {
+//            results.put(key, "added");
+//        }
+//
+//        Set<String> deleted = new HashSet<>(keys1);
+//        deleted.removeAll(keys2);
+//        for (String key: deleted) {
+//            results.put(key, "deleted");
+//        }
+//
+//        Set<String> intersection = new HashSet<>(keys1);
+//        intersection.retainAll(keys2);
+//        for (String key: intersection) {
+//            if (data1.get(key).equals(data2.get(key))) {
+//                results.put(key, "unchanged");
+//            } else {
+//                results.put(key, "changed");
+//            }
+//        }
+//
+//        return results;
+//    }
+
 }
 //END
