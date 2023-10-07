9c565f04ddb17357a9f8dd1a0a4252ab3ee88013
@hexlet/cli <support@hexlet.io>
2023-10-07 21:28:40 +0300

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -0,0 +1,20 @@
+package exercise;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
+// BEGIN
+public class App {
+    public static void save(Path path, Car car) throws IOException {
+        String data = car.serialize();
+        Files.writeString(path, data);
+    }
+
+    public static Car extract(Path path) throws IOException {
+        String data = "";
+        data = Files.readString(path);
+        return Car.unserialize(data);
+    }
+}
+// END
