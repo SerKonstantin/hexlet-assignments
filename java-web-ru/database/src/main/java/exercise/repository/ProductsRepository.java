96f35eb56cd9cead85d059eb26e9b5ee2349f389
@hexlet/cli <support@hexlet.io>
2024-01-15 21:14:47 +0100

    submit java-web-ru/database

---
+++ java-web-ru/database/src/main/java/exercise/repository/ProductsRepository.java
@@ -0,0 +1,64 @@
+package exercise.repository;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import exercise.model.Product;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ProductsRepository extends BaseRepository {
+
+    // BEGIN
+    public static void save(Product product) throws SQLException {
+        String sql = "INSERT INTO products (title, price) VALUES (?, ?)";
+        try (var conn = dataSource.getConnection(); var stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, product.getTitle());
+            stmt.setInt(2, product.getPrice());
+            stmt.executeUpdate();
+            var generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                product.setId(generatedKeys.getLong(1));
+            } else {
+                throw new SQLException("DB have not returned an id after saving an entity");
+            }
+        }
+    }
+
+    public static Optional<Product> find(Long id) throws SQLException {
+        String sql = "SELECT * FROM products WHERE id = ?";
+        try (var conn = dataSource.getConnection(); var stmt = conn.prepareStatement(sql)) {
+            stmt.setLong(1, id);
+            var results = stmt.executeQuery();
+            if (results.next()) {
+                var title = results.getString("title");
+                var price = results.getInt("price");
+                var product = new Product(title, price);
+                product.setId(id);
+                return Optional.of(product);
+            } else {
+                return Optional.empty();
+            }
+        }
+    }
+
+    public static List<Product> getEntities() throws SQLException {
+        var sql = "SELECT * FROM products";
+        try (var conn = dataSource.getConnection(); var stmt = conn.prepareStatement(sql)) {
+            var results = stmt.executeQuery();
+            var products = new ArrayList<Product>();
+            while (results.next()) {
+                var id = results.getLong("id");
+                var title = results.getString("title");
+                var price = results.getInt("price");
+                var product = new Product(title, price);
+                product.setId(id);
+                products.add(product);
+            }
+            return products;
+        }
+    }
+    // END
+}
