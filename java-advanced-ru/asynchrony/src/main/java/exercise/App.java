47c8aee8c08ec4e14239dfe8b1c776ad5a5e454d
@hexlet/cli <support@hexlet.io>
2024-04-30 14:46:25 +0200

    submit java-advanced-ru/asynchrony

--- java-advanced-ru/asynchrony/src/main/java/exercise/App.java
+++ java-advanced-ru/asynchrony/src/main/java/exercise/App.java
@@ -1,12 +1,12 @@
 package exercise;
 
 import java.util.concurrent.CompletableFuture;
-//import java.util.Arrays;
+import java.util.Arrays;
 //import java.nio.file.Paths;
 import java.nio.file.Path;
 import java.nio.file.Files;
-//import java.io.File;
-//import java.nio.file.StandardOpenOption;
+import java.io.File;
+import java.nio.file.StandardOpenOption;
 
 class App {
 
@@ -35,7 +35,7 @@ class App {
         CompletableFuture<String> write1 = read1.thenCombine(read2, (content1, content2) -> {
             String combined = content1 + content2;
             try {
-                Files.writeString(Path.of(pathNameResult), combined);
+                Files.writeString(Path.of(pathNameResult), combined, StandardOpenOption.CREATE);
                 return combined;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -45,6 +45,28 @@ class App {
         write1.join();
         return write1;
     }
+
+    public static CompletableFuture<Long> getDirectorySize(String path) {
+
+        var directory = new File(path);
+
+        if (!directory.isDirectory()) {
+            return CompletableFuture.completedFuture(0L);
+        }
+
+        File[] files = directory.listFiles();
+
+        CompletableFuture<Long>[] fileSizes = Arrays.stream(files)
+                .filter(File::isFile)
+                .map(file -> CompletableFuture.supplyAsync(() -> file.length()))
+                .toArray(CompletableFuture[]::new);
+
+        return CompletableFuture.allOf(fileSizes)
+                .thenApply(v -> Arrays.stream(fileSizes)
+                        .mapToLong(CompletableFuture::join)
+                        .sum());
+    }
+
     // END
 
     public static void main(String[] args) throws Exception {
@@ -55,6 +77,10 @@ class App {
                 "src/main/resources/dest.txt"
         );
         System.out.println(result);
+
+        CompletableFuture<Long> size = getDirectorySize("src/main/resources");
+        result.get();
+        System.out.println(size.get());
         // END
     }
 }
